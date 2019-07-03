f6669d1b72df2eabd4b494e02fde47adb62ebd49
jyotsnab <jyotsna17101@gmail.com>
2019-07-03 17:07:54 +0200

    retrieval alg and corrections module

--- spacesense/retrieval.py
+++ spacesense/retrieval.py
@@ -1,1 +1,30 @@
 import os
+import tensorflow as tf
+
+class RSIF(object)
+    def __init__(self):
+        """
+
+        """
+    def train(self):
+        '''
+
+        :return:
+        '''
+
+    def predict(self):
+        '''
+
+        :return:
+        '''
+    def gentine_lab(self,):
+        '''
+        This algorithm takes reference from the work described in this paper:
+        Gentine, P., & Alemohammad, S. H. (2018). Reconstructed Solar-Induced Fluorescence: A machine learning
+        vegetation product based on MODIS surface reflectance to reproduce GOME-2 solar-induced fluorescence.
+        Geophysical Research Letters, 45. https:// doi.org/10.1002/2017GL076294
+        Same has been developed for matlab by author S. Hamed Alemohammad at https://github.com/HamedAlemo/RSIF
+
+
+        :return:
+        '''
