91b41daa6b2309f5a211c0e68a6368aa0dd30d4f
jyotsnab <jyotsna17101@gmail.com>
2019-07-05 15:35:55 +0200

    debug

--- spacesense/datasets.py
+++ spacesense/datasets.py
@@ -81,16 +81,6 @@ class download_modis(object):
         """
 
 
-class download_modis(object):
-    """
-
-    TBD
-    :return:
-    """
-
-    def __init__(self):
-        """
-        """
 
 
 class read_modis(object):
@@ -115,6 +105,72 @@ class read_modis(object):
         print('dataset saved in .npy format at this location:', save_folder)
 
 
+class read_sentinel(object):
+    def __init__(self, folder_path, img_type='sentinel_2'):
+        self.img_type = img_type
+        self.folder_path = folder_path
+        self.band_files = sorted(glob(self.folder_path + '/*.jp2'))
+        self.band_details = [file.split('_')[-1].split('.')[0] for file in self.band_files]
+        im = gdal.Open(self.band_files[1])
+        arf_base = im.ReadAsArray()
+        self.img_shp = arf_base.shape
+        self.meta_data = im.GetMetadata()
+        self.num_bands = len(self.band_files)
+        self.AOI = None
+        self.data = None
+
+    def get_data(self, bandfiles, row_start=0, col_start=0, row_len=1000, col_len=1000):
+        # loads from npy files for efficiency
+        if bandfiles == None:
+            bandfiles = self.band_files
+        npy_files = sorted(glob(self.folder_path + '/*.npy'))
+        if len(npy_files) == 0:
+            if self.img_type == 'sentinel-2':
+                self.sentinel_2_remap()
+
+        self.row_len = row_len
+        self.col_len = col_len
+        self.row_start = row_start
+        self.col_start = col_start
+        input_bands = bandfiles
+        num_bands = len(input_bands)
+        data = np.zeros((self.row_len, self.col_len, num_bands))
+        # one band info for all pixels loaded in each iteration
+        for i in range(num_bands):
+            arf = np.load(input_bands[i])
+            ap1 = arf[row_start:row_start + row_len, col_start:col_start + col_len]
+            data[:, :, i] = ap1
+
+    def save_as_npy(self, save_folder=os.getcwd()):
+        np.save(save_folder + '/data', self.data)
+        print('dataset saved in .npy format at this location:', save_folder)
+
+    def sentinel_2_remap(self):
+        # load and save all bands as numpy arrays for resuse and remap bands [3,4,5,7,8,9]
+        start_time = time.time()
+        row_, col_ = self.img_shp
+        for b in range(10):
+            if b in [4, 5, 6, 10, 11, 12]:
+                ar_n = np.zeros((row_, col_))
+                im = gdal.Open(self.band_files[b])
+                ar = im.ReadAsArray()
+                name = self.band_details[b] + '.npy'
+                for i in range(int(row_ / 2)):
+                    for j in range(int(col_ / 2)):
+                        n = int(i * 2)
+                        m = int(j * 2)
+                        ar_n[n:n + 2, m:m + 2] = ar[i, j]
+                np.save(self.folder_path + '/' + name, ar_n)
+                print(self.band_details[b], ar_n.shape)
+
+            else:
+                im = gdal.Open(self.band_files[b])
+                ar = im.ReadAsArray()
+                name = self.band_details[b] + '.npy'
+                np.save(self.folder_path + '/' + name, ar)
+                print(self.band_details[b], ar.shape)
+        print('all bands saved as numpy arrays for faster processing in \
+              %s seconds' % (time.time() - start_time))
 
 
     
