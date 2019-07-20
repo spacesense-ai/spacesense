d7d903a463324fb47f356a52d257a38eaee96127
jyotsnab <jyotsna17101@gmail.com>
2019-07-20 19:06:15 +0200

    multiple additions

--- spacesense/datasets.py
+++ spacesense/datasets.py
@@ -68,7 +68,7 @@ class download_sentinel(object):
     def download_files(self, list_product_ids,directory_path='.'):
         for product_id in list_product_ids:
             self.api.download(product_id, directory_path=directory_path)
-    
+
     """ add function to display product AOI Polygon"""
 
 
--- spacesense/preprocessing.py
+++ spacesense/preprocessing.py
@@ -2,3 +2,11 @@ import os
 import numpy as np
 import math
 import sklearn
+import gdal
+
+class corrections(object):
+    def __init__(self):
+        """
+        Class for correction methods basic processing on raw remote sensing data
+        """
+
