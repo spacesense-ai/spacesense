2e25b73e7fe5239d6619209bdad6ac904db8dd59
jyotsnab <jyotsna17101@gmail.com>
2019-08-15 21:20:43 +0200

    debug

--- spacesense/utils.py
+++ spacesense/utils.py
@@ -16,6 +16,12 @@ def get_ndvi(data, nir_index=7, red_index=3, datatype='sentinel-2'):
     NDVI = (b_nir - b_red)/(b_nir + b_red)
     :return: NDVI values for each pixel
     """
-    ndvi = (data[:, :, nir_index] - data[:, :, red_index]) / (data[:, :, nir_index] + data[:, :, red_index])
-
+    if len(data.shape)==3:
+        ndvi = (data[:, :, nir_index] - data[:, :, red_index]) / (data[:, :, nir_index] + data[:, :, red_index])
+    elif len(data.shape)==2:
+        ndvi = (data[:, nir_index] - data[:, red_index]) / (data[:, nir_index] + data[:, red_index])
+    elif len(data.shape)==1:
+        ndvi = (data[nir_index] - data[red_index]) / (data[nir_index] + data[red_index])
+    else:
+        print('Check Input')
     return ndvi
